3e55d75e79dd9ecb90792e972c0faf2782ca2c34
AndOliver46 <anderson_souza75@live.com>
2022-10-25 11:03:51 -0300

    User insert with Post

--- src/main/java/com/andoliver46/workshopmongo/domain/services/UserService.java
+++ src/main/java/com/andoliver46/workshopmongo/domain/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.andoliver46.workshopmongo.domain.User;
 import com.andoliver46.workshopmongo.domain.repository.UserRepository;
 import com.andoliver46.workshopmongo.domain.services.exception.ObjectNotFoundException;
+import com.andoliver46.workshopmongo.dto.UserDTO;
 
 @Service
 public class UserService {
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
