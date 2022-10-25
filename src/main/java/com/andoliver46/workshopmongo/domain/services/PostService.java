1f2e29f9171984142f73480b3b0f6c18086f9ad8
AndOliver46 <anderson_souza75@live.com>
2022-10-25 13:15:48 -0300

    Obtaining post by id

---
+++ src/main/java/com/andoliver46/workshopmongo/domain/services/PostService.java
@@ -0,0 +1,22 @@
+package com.andoliver46.workshopmongo.domain.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andoliver46.workshopmongo.domain.Post;
+import com.andoliver46.workshopmongo.domain.repository.PostRepository;
+import com.andoliver46.workshopmongo.domain.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
