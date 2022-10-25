7fea9e1f421d435a75ad17a3712fda60eb3d94f4
AndOliver46 <anderson_souza75@live.com>
2022-10-25 15:23:38 -0300

    Query with many criteria

--- src/main/java/com/andoliver46/workshopmongo/domain/repository/PostRepository.java
+++ src/main/java/com/andoliver46/workshopmongo/domain/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.andoliver46.workshopmongo.domain.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,10 +11,13 @@ import com.andoliver46.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
-	
-	@Query("{ title: { $regex: ?0, $options: 'i'}}")//{ campo do objeto(Post) a ser procurado: { $regex: posição do objeto nos parametros do metodo, $options: opções auxiliares}}
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i'}}") 
 	List<Post> findByTitle(String text);
-	
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2}}, { $or: [ { 'title': { $regex: ?0, $options: 'i'}}, { 'body': { $regex: ?0, $options: 'i'}}, { 'comments.text': { $regex: ?0, $options: 'i'}} ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+
 }
